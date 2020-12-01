a6e6a368848083722eadb5fd91610b1bc7b0f014
Roderik <39650285+roderikmogot@users.noreply.github.com>
2020-12-01 19:39:01 +0800

    1/12/2020

--- Football/main.cpp
+++ Football/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    char in[100];
+    cin >> in;
+
+    int streak;
+    for(int i = 1; i <= strlen(in); i ++){
+        if(in[i] == in[i-1]){
+            streak ++;
+            if(streak == 7){
+                cout << "YES";
+                return 0;
+            }
+        }
+        else streak = 1;
+    }
+    cout << "NO";
 }
