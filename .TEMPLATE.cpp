40a827215eb9db8ae589eb8eee2efbd042ab27c4
Roderik Mogot <39650285+roderikmogot@users.noreply.github.com>
2020-12-20 12:05:38 +0800

    Create .TEMPLATE.cpp

---
+++ .TEMPLATE.cpp
@@ -0,0 +1,110 @@
+//author: ...
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <chrono>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <cstring>
+#include <unordered_map>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ar array
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define for0(i,n) for(int i=0; i<n; i++)
+#define each(x, a) for (auto& x: a)
+
+template<class A> void read(vt<A>& v);
+template<class A, size_t S> void read(ar<A, S>& a);
+template<class T> void read(T& x) {
+    cin >> x;
+}
+void read(double& d) {
+    string t;
+    read(t);
+    d=stod(t);
+}
+void read(long double& d) {
+    string t;
+    read(t);
+    d=stold(t);
+}
+template<class H, class... T> void read(H& h, T&... t) {
+    read(h);
+    read(t...);
+}
+template<class A> void read(vt<A>& x) {
+    each(a, x)
+        read(a);
+}
+template<class A, size_t S> void read(array<A, S>& x) {
+    each(a, x)
+        read(a);
+}
+template<class A> void write(A x) {
+    cout << to_string(x);
+}
+template<class H, class... T> void write(const H& h, const T&... t) {
+    write(h);
+    write(t...);
+}
+void print() {
+    write("\n");
+}
+template<class H, class... T> void print(const H& h, const T&... t) {
+    write(h);
+    if(sizeof...(t))
+        write(' ');
+    print(t...);
+}
+void DBG() {
+    cerr << "]" << endl;
+}
+template<class H, class... T> void DBG(H h, T... t) {
+    cerr << to_string(h);
+    if(sizeof...(t))
+        cerr << ", ";
+    DBG(t...);
+}
+#ifdef _DEBUG
+#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#else
+#define dbg(...) 0
+#endif
+
+void solve() {
+
+}
+                        
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
