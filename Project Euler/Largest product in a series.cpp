ebe944d9fe7564df5c8cca152430efccc21e4c09
Roderik Mogot <39650285+roderikmogot@users.noreply.github.com>
2021-01-17 18:15:27 +0800

    Update Largest product in a series.cpp

--- Project Euler/Largest product in a series.cpp
+++ Project Euler/Largest product in a series.cpp
@@ -1,4 +1,4 @@
-//author: rod
+//author: .rod.
 //#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
@@ -22,6 +22,8 @@
 #include <fstream>
 #include <numeric>
 #include <sstream>
+#include <random>
+#include <cassert>
 #include <algorithm>
 #include <cmath>
 #include <cstdio>
@@ -37,11 +39,9 @@ using namespace std;
 #define pb push_back
 #define all(c) (c).begin(), (c).end()
 #define sz(x) (int)(x).size()
-#define for0(i,n) for(ll i=0; i<n; i++)
-#define for1(i,n) for(ll i=1; i<n; i++)
-#define for2(i,n) for(ll i=1; i<=n; i++)
+#define for0(i,n) for(int i=0; i<n; i++)
+#define for1(i,n) for(int i=1; i<=n; i++)
 #define each(x, a) for (auto& x: a)
-#define lowercase(x) transform(all(x),x.begin(),::tolower)
 
 #define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
 #define F_OR1(e) F_OR(i, 0, e, 1)
@@ -149,22 +149,26 @@ ll max(ll c, ll d){
     return c < d ? d : c;
 }
 
-/* not working
- */
-
 void solve() {
     string s = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
-    ll mx = 0;
-    for0(i,sz(s)){
-        mx = max(mx,(s[i]-'0')*(s[i+1]-'0')*(s[i+2]-'0')*(s[i+3]-'0'));
+    ll mx = 0, ch;
+    for(int i = 0; i <= sz(s)-1-12; i++){
+        ll store = 1;
+        for(int j = i; j < i+13; j++){
+            ch = s[j]-'0';
+            store *= ch;
+        }
+        mx = max(store, mx);
     }
     print(mx);
     cerr << "time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " sec \n";
+    
+    //cerr << "time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " sec \n";
 }
-                        
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     solve();
     return 0;
 }
