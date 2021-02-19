00b92472cacbef9cb166b819da637c9e323828f4
Roderik Mogot <39650285+roderikmogot@users.noreply.github.com>
2021-02-19 16:21:35 +0800

    Create ... (Triple Dots).java

---
+++ AtCoder/... (Triple Dots).java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        if(s.charAt(s.length()-1)=='2'||s.charAt(s.length()-1)=='4'||s.charAt(s.length()-1)=='5'||s.charAt(s.length()-1)=='7'||s.charAt(s.length()-1)=='9'){
+            System.out.println("hon");
+        }
+        else if(s.charAt(s.length()-1)=='0'||s.charAt(s.length()-1)=='1'||s.charAt(s.length()-1)=='6'||s.charAt(s.length()-1)=='8'){
+            System.out.println("pon");
+        }else{
+            System.out.println("bon");
+        }
+    }
+}
+
+
